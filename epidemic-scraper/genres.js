f1b935875103ec19facf605453c0716bceecd390
ikno-the-box-guy <erwtje013@gmail.com>
2024-12-07 16:36:55 +0100

    Init project

---
+++ epidemic-scraper/genres.js
@@ -0,0 +1,23 @@
+﻿const buildGenreTree = (genres) => {
+    const genreMap = new Map();
+
+    genres.forEach((tag) => {
+        tag.children = [];
+        genreMap.set(tag.id, tag);
+    });
+
+    const tree = [];
+
+    genres.forEach((genre) => {
+        const parentId = genre.fatherGenreId;
+        if (parentId && genreMap.has(parentId)) {
+            genreMap.get(parentId).children.push(genre);
+        } else {
+            tree.push(genre);
+        }
+    });
+
+    return tree;
+}
+
+export default buildGenreTree;
---
+++ epidemic-scraper/index.js
@@ -0,0 +1,12 @@
+﻿import buildGenreTree from "./genres.js";
+
+fetch('https://www.epidemicsound.com/json/tags/?sfx_tag=true').then(response => response.json()).then(data => {
+    // const genres = buildGenreTree(data.genres);
+    
+    let trackCount = 0;
+    data.genres.forEach((genre) => {
+        trackCount += genre.numTracks;
+    });
+    
+    console.log(trackCount)
+});
